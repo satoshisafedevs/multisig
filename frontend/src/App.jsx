a30214aae126aaf50828e711498280fc73bd46f3
Ilia Kislukhin <ilia@getprontoai.com>
2023-02-07 23:05:07 +0000

    Upload New File

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        {/* <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/topics">
+          <Topics />
+        </Route> */}
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
