abca2fdb6c8c9f01525347ac42f8022ebf86aa6b
Ilia Kislukhin <ilia@getprontoai.com>
2023-02-09 14:40:21 +0000

    add prettier config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,21 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {
+    getAuth,
+    RecaptchaVerifier,
+    signInWithPhoneNumber,
+} from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD5FtVkUOqufdpZu8FhaFWJHzBECXDn6_k',
-  authDomain: 'text-chatgpt.firebaseapp.com',
-  projectId: 'text-chatgpt',
-  storageBucket: 'text-chatgpt.appspot.com',
-  messagingSenderId: '1041401476799',
-  appId: '1:1041401476799:web:04a9c8384dc0dc0b99780a',
-  measurementId: 'G-90JE35NMRL',
+    apiKey: "AIzaSyD5FtVkUOqufdpZu8FhaFWJHzBECXDn6_k",
+    authDomain: "text-chatgpt.firebaseapp.com",
+    projectId: "text-chatgpt",
+    storageBucket: "text-chatgpt.appspot.com",
+    messagingSenderId: "1041401476799",
+    appId: "1:1041401476799:web:04a9c8384dc0dc0b99780a",
+    measurementId: "G-90JE35NMRL",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
-export {
-  app, auth, analytics, RecaptchaVerifier, signInWithPhoneNumber,
-};
+export { app, auth, analytics, RecaptchaVerifier, signInWithPhoneNumber };
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-import Home from './routes/Home';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
+    {
+        path: "/",
+        element: <Home />,
+    },
 ]);
 
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ChakraProvider>
+            <RouterProvider router={router} />
+        </ChakraProvider>
+    </React.StrictMode>,
 );
