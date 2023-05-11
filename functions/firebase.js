93e4003487c2d00441a5e035995e8814daa08d70
Indy The Oracle <indytheoracle@protonmail.com>
2023-05-10 20:42:22 -0700

    added state machine and v2 functions

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,9 +1,11 @@
 // firebase.js
 const admin = require("firebase-admin");
+const { onRequest } = require("firebase-functions/v2/https");
 admin.initializeApp();
 
 const db = admin.firestore();
 
 module.exports = {
     db,
+    onRequest,
 };
