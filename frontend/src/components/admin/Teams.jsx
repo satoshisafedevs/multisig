a096143eae51d953de4c72c1a54c31ff67ddf959
Matthew Moravec <matt@getprontoai.com>
2023-08-05 20:31:05 +0000

    Teams page initial

---
+++ frontend/src/components/admin/Teams.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Flex, Heading, Text, Button } from "@chakra-ui/react";
+import { IoExitOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../../providers/User";
+import AddSatoshiSafeModal from "./AddSatoshiSafeModal";
+
+function Teams() {
+    const { teamsData, leaveTeam, currentTeam } = useUser();
+    const [modalOpen, setModalOpen] = useState(false);
+    const { navigate } = useNavigate();
+
+    const handleLeaveTeam = (team) => {
+        // your logic to handle leaving the team goes here
+        leaveTeam(team);
+        if (team.id === currentTeam.id) {
+            navigate("/");
+        }
+    };
+
+    return (
+        <Box padding="10px" minWidth="500px" margin="10px">
+            <Flex justifyContent="space-between" alignItems="center" mb="20px">
+                <Box>
+                    <Heading as="h2" mb="10px">
+                        Teams
+                    </Heading>
+                    <Text>Manage the teams associated with your account</Text>
+                </Box>
+            </Flex>
+            <Box>
+                <Table>
+                    <Thead>
+                        <Tr>
+                            <Th>Team Name</Th>
+                            <Th>Users</Th>
+                            <Th>Actions</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {teamsData.map((team) => (
+                            <Tr key={team.name}>
+                                <Td>{team.name}</Td>
+                                <Td>{team.users.length}</Td>
+                                <Td>
+                                    <Button
+                                        rightIcon={<IoExitOutline size="20px" />}
+                                        colorScheme="red"
+                                        variant="outline"
+                                        onClick={() => handleLeaveTeam(team)}
+                                    >
+                                        Leave Team
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+                <AddSatoshiSafeModal isOpen={modalOpen} setIsOpen={setModalOpen} />
+            </Box>
+        </Box>
+    );
+}
+
+export default Teams;
--- frontend/src/providers/User.jsx
+++ frontend/src/providers/User.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useMemo, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useToast } from "@chakra-ui/react";
-import { auth, onAuthStateChanged, db, collection, doc, getDoc, getDocs, updateDoc } from "../firebase";
+import { auth, onAuthStateChanged, db, collection, doc, getDoc, getDocs, updateDoc, deleteDoc } from "../firebase";
 
 const UserContext = createContext();
 const UserProvider = UserContext.Provider;
@@ -104,6 +104,26 @@ function User({ children }) {
         }
     };
 
+    const leaveTeam = async (team) => {
+        try {
+            const userTeamRef = doc(db, "users", user.uid, "teams", team.id);
+            deleteDoc(userTeamRef);
+            const teamRef = doc(db, "teams", team.id);
+            const getTeamSnap = await getDoc(teamRef);
+            updateDoc(teamRef, {
+                users: getTeamSnap.data().users.filter((userUid) => userUid !== user.uid),
+            });
+        } catch (error) {
+            toast({
+                description: `Failed to leave the team: ${error.message}`,
+                position: "top",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    };
+
     const values = useMemo(
         () => ({
             user,
@@ -122,6 +142,7 @@ function User({ children }) {
             getUserTeamsData,
             getFirestoreUserData,
             setUserTeamWallet,
+            leaveTeam,
         }),
         [
             user,
@@ -140,6 +161,7 @@ function User({ children }) {
             getUserTeamsData,
             getFirestoreUserData,
             setUserTeamWallet,
+            leaveTeam,
         ],
     );
 
