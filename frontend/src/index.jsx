27dec73ed3e72341b8e560dc7bf40d10743a7ecd
Łukasz Szmidt <lukaszxszmidt@gmail.com>
2024-04-03 17:53:31 +0000

    Sentry - Added sentry integration

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,14 @@
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+Sentry.init({
+    dsn: "https://8cd42e7a5d1c83472371ac44b18ae973@o4507022754381824.ingest.us.sentry.io/4507022758051840",
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    tracePropagationTargets: ["https://playground.satoshisafe.ai", "https://app.getsatoshisafe.com"],
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
