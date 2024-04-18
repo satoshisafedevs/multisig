cb44450b6b34ead90e73e289081257fe2d54d012
Ilia Kislukhin <ilia@getprontoai.com>
2024-04-18 18:11:21 -0500

    Fix font import

--- frontend/src/components/Fonts.jsx
+++ frontend/src/components/Fonts.jsx
@@ -3,28 +3,34 @@ import { Global } from "@emotion/react";
 
 function Fonts() {
     return (
-        <Global
-            styles={`
-
-                /* Import the Google font 'Adamina' */
-                @import url('https://fonts.googleapis.com/css2?family=Adamina&display=swap');
-
-                @font-face {
-                    font-family: 'MontrealBook';
-                    font-style: normal;
-                    font-weight: 400;
-                    font-display: swap;
-                    src: url("/fonts/PPNeueMontreal-Book.woff2") format('woff2');
-                }
-                @font-face {
-                    font-family: 'MontrealMedium';
-                    font-style: normal;
-                    font-weight: 700;
-                    font-display: swap;
-                    src: url('/fonts/PPNeueMontreal-Medium.woff2') format('woff2');
-                }
+        <>
+            {/* Import the Google font 'Adamina' as a separate Global component */}
+            <Global
+                styles={`
+                    @import url('https://fonts.googleapis.com/css2?family=Adamina&display=swap');
                 `}
-        />
+            />
+
+            {/* Define custom fonts using @font-face in another Global component */}
+            <Global
+                styles={`
+                    @font-face {
+                        font-family: 'MontrealBook';
+                        font-style: normal;
+                        font-weight: 400;
+                        font-display: swap;
+                        src: url("/fonts/PPNeueMontreal-Book.woff2") format('woff2');
+                    }
+                    @font-face {
+                        font-family: 'MontrealMedium';
+                        font-style: normal;
+                        font-weight: 700;
+                        font-display: swap;
+                        src: url('/fonts/PPNeueMontreal-Medium.woff2') format('woff2');
+                    }
+                `}
+            />
+        </>
     );
 }
 
